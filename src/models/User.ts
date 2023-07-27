a463b2d5ea9bee41b6d4f29dace75edc7c9062d9
Tomas Lajda <tomas.lajda0@gmail.com>
2023-07-27 23:12:55 +0200

    Creating setters and getters with composition style

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,24 +1,21 @@
+import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-type KeyName = string | number | undefined;
-
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
-  [key: string]: KeyName;
 }
+
+const rootUrl = 'http://localhost:3000/users';
 
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  constructor(private data: UserProps) {}
-
-  get(propName: string): KeyName {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
